a9a52d30722da491330db6605529ed4ea2dd71a2
Nicolas Marzullo <marzullonicolas@gmail.com>
2018-11-19 14:07:51 -0300

    Acomodo packages

--- src/algoritmo/Grafo.java
+++ src/algoritmo/Grafo.java
@@ -30,25 +30,26 @@ public class Grafo {
 		// Cargo la cola con el nodo solicitado
 		int nodoW, costoDirecto, costoConNodoIntermedio;
 
-		//Cargo los costos de las aristas en la cola de prioridad
+		// Cargo los costos de las aristas en la cola de prioridad
 		for (int j = 0; j < this.cantidadDeNodos; j++) {
 			if (j != nodoSolicitado) // no quiero cargar el costo para llegar a sigo mismo
 				this.colaCostoArista
 						.add(new Arista(this.matrizDeCostos.getValor(nodoSolicitado, j), nodoSolicitado, j));
 		}
 
-		//Cargo el vector de costos
+		// Cargo el vector de costos
 		for (int i = 0; i < this.cantidadDeNodos; i++) {
 			costosDelNodoSolicitadoAN[i] = this.matrizDeCostos.getValor(nodoSolicitado, i);
 		}
 
-		//Mientras queden elementos en la cola por analizar. (Conjunto v-s)
+		// Mientras queden elementos en la cola por analizar. (Conjunto v-s)
 		while (!this.colaCostoArista.isEmpty()) {
-			
-			//La cola me saca el mejor.
+
+			// La cola me saca el mejor.
 			nodoW = this.colaCostoArista.poll().nodoDestino;
 
-			// Cargo los adyacentes. Sé que un nodo no es adyacente con otro si tiene un infinito.
+			// Cargo los adyacentes. Sé que un nodo no es adyacente con otro si tiene un
+			// infinito.
 			for (int i = 0; i < this.cantidadDeNodos; i++) {
 				if (this.matrizDeCostos.getValor(nodoW, i) != INFINITO) {
 					listaDeAdyacentes.add(i);
@@ -56,21 +57,23 @@ public class Grafo {
 			}
 
 			for (Integer i : listaDeAdyacentes) {
-				
-				//Calculo los costos yendo directamente al nodo o pasando por un intermedio a ver cual es mejor
+
+				// Calculo los costos yendo directamente al nodo o pasando por un intermedio a
+				// ver cual es mejor
 				costoDirecto = costosDelNodoSolicitadoAN[i];
 				costoConNodoIntermedio = costosDelNodoSolicitadoAN[nodoW] + this.matrizDeCostos.getValor(nodoW, i);
 
 				if (costoConNodoIntermedio < costoDirecto) {
 					costosDelNodoSolicitadoAN[i] = costoConNodoIntermedio;
 
-					//Esto es para actualizar la cola, ya que solo se puede actualizar eliminando y volviendo a agregar el elemento
+					// Esto es para actualizar la cola, ya que solo se puede actualizar eliminando y
+					// volviendo a agregar el elemento
 					colaCostoArista.remove(new Arista(costoDirecto, nodoSolicitado, i));
 					colaCostoArista.add(new Arista(costoConNodoIntermedio, nodoSolicitado, i));
 				}
 
 			}
-			//No olvidar de limpiar los adyacentes!!
+			// No olvidar de limpiar los adyacentes!!
 			listaDeAdyacentes.clear();
 
 		}
